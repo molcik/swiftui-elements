e0964b5307ed40878452c666b2ff64c3cdcb52b6
Filip <filipkrayem@gmail.com>
2021-06-07 18:30:56 +0200

    Places Map ignores safe area

--- sketch-elements/Photography/Places.swift
+++ sketch-elements/Photography/Places.swift
@@ -25,7 +25,7 @@ struct Places: View {
                     PhotoStack(numberOfPhotos: item.photos.count, showcasedPhotos: Array(item.photos.prefix(3)), hidden: region.span.latitudeDelta / 2 > 30)
                         .frame(width: 100 - CGFloat(region.span.latitudeDelta / 2), height: 100 - CGFloat(region.span.longitudeDelta / 1.5), alignment: .center)
                 }
-            }
+            }.ignoresSafeArea()
         }
     }
 }
